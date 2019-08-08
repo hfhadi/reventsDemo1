c685935b3edcf95e1fd66e205a41ec051292d68d
Haydar <hfhadi@gmail.com>
2019-08-08 18:03:33 -0400

    48 Internal Navigation for our application

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu, Image, Dropdown } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const SignedInMenu = ({signOut}) => {
   return (
@@ -11,7 +12,7 @@ const SignedInMenu = ({signOut}) => {
           <Dropdown.Item text="My Events" icon="calendar" />
           <Dropdown.Item text="My Network" icon="users" />
           <Dropdown.Item text="My Profile" icon="user" />
-          <Dropdown.Item text="Settings" icon="settings" />
+          <Dropdown.Item text="Settings" icon="settings" as={Link} to="/settings" />
           <Dropdown.Item text="Sign Out" icon="power" onClick={signOut} />
         </Dropdown.Menu>
       </Dropdown>
