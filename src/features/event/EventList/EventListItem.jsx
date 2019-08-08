701b6d89e717bc2276f0c6cbdfe0050e056f8594
Haydar <hfhadi@gmail.com>
2019-08-08 09:54:11 -0400

    40 Deleting the events

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -4,7 +4,7 @@ import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
   render() {
-    const { event, selectEvent } = this.props;
+    const { event, selectEvent, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -34,8 +34,17 @@ class EventListItem extends Component {
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
+
           <Button
-            onClick={(evt) => selectEvent(evt, event)}
+            onClick={() => deleteEvent(event.id)}
+            as="a"
+            color="red"
+            floated="right"
+            content="Delete"
+          />
+
+          <Button
+            onClick={evt => selectEvent(evt, event)}
             as="a"
             color="teal"
             floated="right"
