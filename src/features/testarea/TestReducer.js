b5a7537d92cb5e82d109c1f2ccc0f77359a36a8c
Haydar Hadi <haydar.hadi@ibm.com>
2019-08-08 23:19:23 -0400

    57 Improving the dev experince

--- src/features/testarea/TestReducer.js
+++ src/features/testarea/TestReducer.js
@@ -1,15 +1,15 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./TestConstants";
 
 const initialState = {
-  data: 42
+  data: 43
 };
 
 const testReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT_COUNTER:
-      return { ...state, data: state.data + 1 };
+      return { ...state, data: state.data + 222 };
     case DECREMENT_COUNTER:
-      return { ...state, data: state.data - 1 };
+      return { ...state, data: state.data - 333 };
     default:
       return state;
   }
