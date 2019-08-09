76d4d961e359e88463a12dcb1ebaad98d4b59368
Haydar Hadi <haydar.hadi@ibm.com>
2019-08-09 06:14:46 -0400

    add comments

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {incrementCounter, decrementCounter} from './TestActions';
 import { Button } from 'semantic-ui-react';
 
-// a.k.a
+// a.k.a mapStateToProps
 const mapState = (state) => ({
     data: state.test.data
 })
