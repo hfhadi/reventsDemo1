03e0b42765bf72396499d95c6cdcb90d62ca526a
Haydar Hadi <haydar.hadi@ibm.com>
2019-08-09 10:29:03 -0400

    70 Fixing the scroll issue on routing to a new pg

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/common/util/ScrollToTop";
 
 const store = configureStore();
 
@@ -17,7 +18,9 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />  
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
     rootEl
